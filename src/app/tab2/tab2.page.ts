d8881796343cfce8fe6f6ed7c8b3f5bdc79903e6
Kinitto <joaquingomezinfor@gmail.com>
2022-01-14 12:38:35 +0100

    galeria

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
+import { PhotoService } from '../services/photo.service';
 
 @Component({
   selector: 'app-tab2',
@@ -7,6 +9,12 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  constructor(public photoService: PhotoService) { }
 
+  addPhotoToGallery() {
+    this.photoService.addNewToGallery();
+
+
+
+  }
 }
