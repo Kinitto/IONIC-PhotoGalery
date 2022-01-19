e0cd8bb39660332473b13c274f02463a66c6735c
Kinitto <joaquingomezinfor@gmail.com>
2022-01-19 14:02:15 +0100

    android

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Camera, CameraResultType, CameraSource, Photo } from '@capacitor/camera';
-import { Filesystem, Directory } from '@capacitor/filesystem';
-import { Storage } from '@capacitor/storage';
+
 import { UserPhoto } from '../interfaces/photointerface';
 
 
@@ -11,7 +10,7 @@ import { UserPhoto } from '../interfaces/photointerface';
 export class PhotoService {
   public photos: UserPhoto[] = [];
 
-  constructor() { }
+  constructor() {}
 
 
   public async addNewToGallery() {
